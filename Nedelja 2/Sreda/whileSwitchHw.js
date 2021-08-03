458824440416a7ae64dfecd04a2dc060590abebf
MiroslavZadrepko <miroslav.zadrepko@gmail.com>
2021-08-03 18:18:36 +0200

    domaci 030821

--- Nedelja 2/Sreda/whileSwitchHw.js
+++ Nedelja 2/Sreda/whileSwitchHw.js
@@ -17,9 +17,21 @@ Na kraju treba da se ispise poruka na primer:
 Puz se popeo na drvo za 8 dana
 
 Smatrati da drvo svakog dana raste fiksno 1cm.
-*/
 
-/*
+
+let predjeniPut = 0;
+let visinaDrveta = 100;
+let dan = 0;
+
+while (predjeniPut<visinaDrveta) {
+    predjeniPut += 3;
+    visinaDrveta ++;
+    dan ++
+    console.log(`Dan ${dan}: Puž je prešao ${predjeniPut} cm, visina drveta je ${visinaDrveta} cm.`);
+}
+console.log(`Puž se popeo za ${dan} dana.`);
+
+*//*
 2. Treba izracunati poptust U DINARIMA koji ostvaruje kupac.
 
 Pravila za ostvarivanje popusta su sledeca:
@@ -41,3 +53,60 @@ dakle racuna se 5% od 8000 din a ne od pocetnog iznosa
 i dobije se dodatni popust od 400.
 Tako da je na kraju ukupan popust 2000 + 400 = 2400 dinara
 */
+
+//kejs zaposleni, popust je ..., iznos je 
+
+let iznos = 10000;
+let popust, dodatniPopust, racun, konacniRacun;
+let greska = `Pogrešan unos jedne ili više vrednosti`;
+let kupac = prompt("Da li je kupac zaposleni, student, penzioner, firma ili nezaposlen?");
+let dan = prompt("Da li je radni dan ili vikend?")
+
+if (kupac == '' || kupac == null || dan == '' || dan == null || kupac != NaN || dan != NaN) {
+    kupac = greska;
+    dan = greska;
+    console.log(`${greska}`);
+
+} else {
+
+switch (kupac) {
+    case "zaposleni":
+        popust = (iznos > 5000) ? ((iznos / 100) * 5) : 0;
+        racun = iznos - popust;
+        console.log(`Popust po ovom osnovu izosi ${popust}. Racun iznosi ${racun}`);
+        break;
+
+    case "student":
+        popust = (iznos > 3000) ? ((iznos / 100) * 20) : 0;
+        racun = iznos - popust;
+        console.log(`Popust po ovom osnovu izosi ${popust}. Racun iznosi ${racun}`);
+        break;
+
+    case "penzioner":
+        popust = (iznos > 2000) ? ((iznos / 100) * 30) : 0;
+        racun = iznos - popust;
+        console.log(`Popust po ovom osnovu izosi ${popust}. Racun iznosi ${racun}`);
+        break;
+
+    case "firma":
+        popust = (iznos > 10000) ? ((iznos / 100) * 10) : 0;
+        racun = iznos - popust;
+        console.log(`Popust po ovom osnovu izosi ${popust}. Racun iznosi ${racun}`);
+        break;
+
+    case "nezaposlen":
+        popust = 0;
+        racun = iznos-popust;
+        console.log(`Ne ostvarujete pravo na popust po ovom osnovu. Racun iznosi ${racun}`);
+        break;
+
+    default:
+        console.log(`ovo je default poruka zarad testa`);
+        break;
+}
+
+dodatniPopust = (dan == "radni dan") ? 0 : ((racun / 100) * 5);
+konacniRacun = racun - dodatniPopust;
+
+console.log(`Konačan iznos za plaćanje, umanjen za vikend popust, iznosi ${konacniRacun}`);
+}
